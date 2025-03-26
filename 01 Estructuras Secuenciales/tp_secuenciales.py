367e9b499a66c8eb06521c2bff63d18de333ca70
gabox2002 <gabox2002@gmail.com>
2025-03-26 12:54:02 -0300

    agregado solucion tp1

---
+++ 01 Estructuras Secuenciales/tp_secuenciales.py
@@ -0,0 +1,109 @@
+#TP SECUENCIALES - Gabriel Quispe DNI 95739343 
+
+# 1) Programa que imprime "Hola Mundo!"
+
+#print ("Hola mundo!")
+
+
+# 2) Programa que saluda al usuario
+
+# nombre = input("Por favor, ingresa tu nombre: ")
+# print(f"Hola {nombre}!")
+
+
+# 3) Programa que recopila información del usuario y la imprime
+
+# nombre = input("Ingresa tu nombre: ")
+# apellido = input("Ingresa tu apellido: ")
+# edad = int(input("Ingresa tu edad: "))
+# residencia = input("Ingresa tu país de residencia: ")
+
+# print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}.")
+
+
+# 4) Programa que pide el radio e imprime su area y su perimetro
+
+# pi = 3.14159
+# radio = float(input("Introduce el radio del círculo: "))
+
+# area = pi * radio ** 2
+# perimetro = 2 * pi * radio
+
+# print(f"El área del círculo es: {area:.2f}")
+# print(f"El perímetro del círculo es: {perimetro:.2f}")
+
+
+# 5) Programa que convierte segundos a horas
+
+# segundos = int(input("Ingrese la cantidad de segundos: "))
+
+# horas = segundos / 3600  # 1 hora = 3600 segundos
+
+# print(f"{segundos} segundos equivalen a {horas:.2f} horas.")
+
+
+# 6) Programa que imprime la tabla de multiplicar de un número dado
+
+# numero = int(input("Introduce un número para ver su tabla de multiplicar: "))
+
+# print(f"Tabla de multiplicar del {numero}:")
+# print(f"{numero} x 1 = {numero * 1}")
+# print(f"{numero} x 2 = {numero * 2}")
+# print(f"{numero} x 3 = {numero * 3}")
+# print(f"{numero} x 4 = {numero * 4}")
+# print(f"{numero} x 5 = {numero * 5}")
+# print(f"{numero} x 6 = {numero * 6}")
+# print(f"{numero} x 7 = {numero * 7}")
+# print(f"{numero} x 8 = {numero * 8}")
+# print(f"{numero} x 9 = {numero * 9}")
+# print(f"{numero} x 10 = {numero * 10}")
+
+
+# 7) Programa que pide dos números enteros distintos de cero y hacer operaciones aritmeticas
+
+# numero1 = int(input("Introduce el primer número: "))
+# numero2 = int(input("Introduce el segundo número: "))
+
+# if numero1 != 0 and numero2 != 0:
+#     suma = numero1 + numero2
+#     resta = numero1 - numero2
+#     multiplicacion = numero1 * numero2
+#     division = numero1 / numero2
+
+#     print(f"Los resultado de las operaciones con los números {numero1} y {numero2}:")
+#     print(f"La suma es: {suma}")
+#     print(f"La resta es: {resta}")
+#     print(f"La multiplicación es: {multiplicacion}")
+#     print(f"La división es: {division}")
+# else:
+#     print("Ambos números deben ser distintos de 0")
+
+
+# 8) Programa que solicita el peso y la altura al usuario y calcula el IMC
+
+# peso = float(input("Introduce tu peso en kilos: "))
+# altura = float(input("Introduce tu altura en metros: "))
+
+# imc = peso / (altura ** 2)
+
+# print(f"Tu índice de masa corporal (IMC) es: {imc:.2f}")
+
+
+# 9) Programa que convierte grados Celsius a Fahrenheit
+
+# temp_celsius = float(input("Introduce la temperatura en grados Celsius: "))
+
+# temp_fahrenheit = (9/5) * temp_celsius + 32
+
+# print(f"La temperatura en grados Fahrenheit es: {temp_fahrenheit:.2f}°F")
+
+
+# 10) Programa que calcula el promedio de tres números
+
+# numero1 = float(input("Introduce el primer número: "))
+# numero2 = float(input("Introduce el segundo número: "))
+# numero3 = float(input("Introduce el tercer número: "))
+
+# promedio = (numero1 + numero2 + numero3) / 3
+
+# print(f"El promedio de los tres números es: {promedio:.2f}")
